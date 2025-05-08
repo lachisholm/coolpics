8f9449fb822477bd5436eb1f79a0dbb4bacb2391
Lora Chisholm <chi20006@byui.edu>
2025-05-08 14:05:08 -0600

    fix

--- coolpics.js
+++ coolpics.js
@@ -1,6 +1,9 @@
+
+
 const menuButton = document.getElementById("menu-button");
+
 const navMenu = document.getElementById("nav-menu");
 
 menuButton.addEventListener("click", () => {
-    navMenu.classList.toggle("open")
+    navMenu.classList.toggle("open");
 });
